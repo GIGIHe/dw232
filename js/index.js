da535f68311a4368c0db582eec8a811dbcd7b51f
GIGIHe <2697190181@qq.com>
2019-07-03 13:45:16 +0800

    1212

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(function(){
+    $(".mfol li").click(function(){
+        $(this).addClass('hover').siblings('li').removeClass('hover');
+        $(".mfexa_type").eq($(this).index()).stop().slideDown(600).siblings(".mfexa_type").hide();
+    })
+
+    $(".point1").click(function(){
+        $("html , body").animate({scrollTop:$(".m1").offset().top},400);
+    });
+
+    $(window).scroll(function(){
+        // 注入ht
+        var top = $(".zgm1").offset().top;//获取指定位置
+        var scrollTop = $(window).scrollTop();
+        if(scrollTop>top){
+            $(".navbar").addClass('active');
+        }else{
+            $(".navbar").removeClass('active');
+        }
+    })
+
+    $(".hjul li").each(function(index){
+        $(this).click(function(){
+            $(this).addClass('hover').siblings('li').removeClass('hover');
+             $(".hjul_inner").hide().eq(index).fadeIn(200);
+        })
+    })
+    $(".tabdl dd").each(function(index){
+        $(this).click(function(){
+            $(this).addClass('hover').siblings('dd').removeClass('hover');
+             $(".table").hide().eq(index).fadeIn(200);
+        })
+    })
+})
